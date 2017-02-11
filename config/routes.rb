a88dae9fd4c3725dd1345723e50f4d497227ff94
Yuki Kuroshima <yuuki3327@hotmail.com>
2017-02-11 07:29:38 +0000

    destroy Home controller and fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'home/index'
-
   devise_for :users
   resources :posts
   resources :users
   
-  root 'home#index'
-  get 'posts/index'
+  root 'posts#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
