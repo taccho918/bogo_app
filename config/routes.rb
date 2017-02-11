33e5f57b288d7abaef8aebb06fa72c9d6ee0e45b
Yuki Kuroshima <yuuki3327@hotmail.com>
2017-02-11 09:13:38 +0000

    add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => {
+ :registrations => 'users/registrations',
+ :sessions => 'users/sessions'
+}
   resources :posts
   resources :users
   
   root 'posts#index'
+  #get '/users/:id' to: 'users#show'
+
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
